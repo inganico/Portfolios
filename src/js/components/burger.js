e9ac33b0078537977101771d4beea90cbed34e49
Seltikc <seltikc@yandex.ru>
2023-11-09 21:49:45 +0500

    begin mobile

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,12 +1,24 @@
 const burgerOpen = document.querySelector('.burger');
 const menuBurger = document.querySelector('.header')
+const html = document.querySelector("html")
+const navLink = document.querySelectorAll('.nav__link')
 
 
 burgerOpen.addEventListener('click', function () {
   menuBurger.classList.toggle('opens');
+  html.classList.add('unscroll')
 })
 
-let html = document.querySelector("html")
-document.querySelector(".burger").onclick = function(){
-html.classList.toggle("unscroll")
-}
+navLink.forEach(elem => {
+
+  elem.addEventListener('click', () => {
+    if (true) {
+      html.classList.remove('unscroll')
+      menuBurger.classList.remove('opens');
+    }
+  })
+})
+
+
+
+
